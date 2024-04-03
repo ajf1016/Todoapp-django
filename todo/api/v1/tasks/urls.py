d9be26dde64503247b4576e0985e646bcf3dba06
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-04-03 12:40:34 +0530

    Creating post request

--- todo/api/v1/tasks/urls.py
+++ todo/api/v1/tasks/urls.py
@@ -4,4 +4,5 @@ from api.v1.tasks import views
 
 urlpatterns = [
     path('', views.tasks),
+    path('/create/', views.create_task),
 ]
--- todo/api/v1/tasks/views.py
+++ todo/api/v1/tasks/views.py
@@ -8,5 +8,25 @@ from api.v1.tasks.serializers import TaskSerializers
 @api_view(['GET'])
 def tasks(request):
     instances = Task.objects.filter(is_deleted=False)
-    serializer = TaskSerializers(instances,many=True)
+    serializer = TaskSerializers(instances, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_task(request):
+    serializer = TaskSerializers(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        response_data = {
+            'status_code': 6000,
+            'message': 'Task created successfully',
+            'data': serializer.data,
+        }
+        return Response(response_data)
+
+    response_data = {
+        'status_code': 6001,
+        'message': 'Task creation is failed',
+        'data': serializer.errors,
+    }
+    return Response(response_data)
